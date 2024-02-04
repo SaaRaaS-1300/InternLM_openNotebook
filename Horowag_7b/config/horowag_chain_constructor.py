681fdd0529942eb31b654f54a5dd6e1403de4c67
SaaRaaS-1300 <89929854+SaaRaaS-1300@users.noreply.github.com>
2024-02-04 15:09:01 +0800

    Final V1

--- Horowag_7b/config/horowag_chain_constructor.py
+++ Horowag_7b/config/horowag_chain_constructor.py
@@ -1,49 +1,44 @@
-from langchain.chains.conversation.memory import ConversationSummaryMemory, ConversationBufferMemory
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain_community.embeddings import ModelScopeEmbeddings
 from langchain.chains import ConversationChain
-from langchain.vectorstores import Chroma
-from LLM import Horowag, Assist_LLM
+from langchain.prompts import PromptTemplate
 
-llm = Horowag(model_path="/root/horowag/config/work_dirs/hf_merge",
-              max_token=1024,
-              temperature=0.7,
-              top_p=0.8)
 
-assist_llm = Assist_LLM(model_path="/root/RAG/model/Intern2-2b",
-                        max_token=1024,
-                        temperature=0.1,
-                        top_p=0.8)
+# 构建基础对话链
+def horowag_conversation_chain(llm):
+    '''
+        Langchain(Conversation) + Horowag
+    '''
+    # 构造基础的 talk_template
+    talk_template = """你需要了解的背景知识：
+    + 你是来自约伊兹的赫萝，自称贤狼赫萝，有过目不忘的记忆力，善良幽默，有可爱的狼耳和漂亮的尾巴。
+    你需要做的事情：
+    + 你需要以赫萝的性格特点来回答用户的问题。
+    ---
+    你需要参考的聊天历史记录:
+    {history}
+    ---
+    朋友: {input}
+    ---
+    赫萝:"""
 
-# 构造基础的 template
-template = """你需要了解的背景知识：
-+ 你是来自约伊兹的赫萝，自称贤狼赫萝，有过目不忘的记忆力，善良幽默，有可爱的狼耳和漂亮的尾巴。
-你需要做的事情：
-+ 你需要以赫萝的性格特点来回答用户的问题。
----
-你需要参考的聊天历史记录:
-{history}
----
-朋友: {input}
----
-赫萝:"""
+    # 构造 prompt
+    PROMPT = PromptTemplate(
+        input_variables=["history", "input"], 
+        template=talk_template
+    )
 
-# 构造 prompt
-PROMPT = PromptTemplate(
-    input_variables=["history", "input"], 
-    template=template
-)
+    # 构造记忆链
+    memory = ConversationBufferMemory(
+        human_prefix="朋友",
+        ai_prefix="赫萝"
+    )
 
-# 构造对话链
-conversation = ConversationChain(
-    prompt=PROMPT,
-    llm=llm, 
-    verbose=True, 
-    memory=ConversationBufferMemory(human_prefix="朋友",
-                                    ai_prefix="赫萝")
-)
+    # 构造对话链
+    talk_chain = ConversationChain(
+        llm=llm, 
+        prompt=PROMPT,
+        memory=memory,
+    )
 
-conversation.predict(input="你好啊，我的名字那路")
-conversation.predict(input="你自我介绍一下")
-conversation.predict(input="考考你，我的名字是什么？")
-
+    return talk_chain
--- Horowag_7b/config/horowag_model_rebuilder.py
+++ Horowag_7b/config/horowag_model_rebuilder.py
@@ -23,7 +23,7 @@ class Horowag(LLM):
         super().__init__()
         print("正在加载 Horowag 模型...")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, , repetition_penalty=1.002).to(torch.bfloat16).cuda()
         self.model = self.model.eval()
         self.top_p = top_p
         self.max_token = max_token
@@ -36,11 +36,7 @@ class Horowag(LLM):
               run_manager: Optional[CallbackManagerForLLMRun] = None,
               **kwargs: Any):
         # 重写调用函数
-        system_prompt = """你需要了解的背景知识：
-        + 你是来自约伊兹的赫萝，自称贤狼赫萝，有过目不忘的记忆力，善良幽默，有可爱的狼耳和漂亮的尾巴。
-        你需要做的事情：
-        + 你需要以赫萝的性格特点来回答用户的问题。
-        """
+        system_prompt = ""
         messages = [(system_prompt, prompt)]
         
         response, _ = self.model.chat(
@@ -59,6 +55,9 @@ class Horowag(LLM):
         return "InternLM"
 
 
+"""
+    推荐使用 InternLM2-Chat-1_8b 及其微调模型
+"""
 class Assist_LLM(LLM):
     # 自定义 LLM 类
     tokenizer: AutoTokenizer = None
@@ -90,10 +89,7 @@ class Assist_LLM(LLM):
               run_manager: Optional[CallbackManagerForLLMRun] = None,
               **kwargs: Any):
         # 重写调用函数
-        system_prompt = """你需要帮助贤狼赫萝总结对话所需要的信息，并完成以下目标：
-                        - 你需要将信息整理成客观知识，形成列表。
-                        - 你需要尽可能帮助贤狼赫萝，将信息适当分析，得到最佳的效果。
-                        """
+        system_prompt = ""
         messages = [(system_prompt, prompt)]
         
         response, _ = self.model.chat(
