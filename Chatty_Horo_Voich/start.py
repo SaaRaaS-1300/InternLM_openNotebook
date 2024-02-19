0be6fc882589d06e17873a61f3164d3a1764dd5c
SaaRaaS-1300 <89929854+SaaRaaS-1300@users.noreply.github.com>
2024-02-20 00:26:25 +0800

    ☃️版本例行维护☃️

--- Chatty_Horo_Voich/start.py
+++ Chatty_Horo_Voich/start.py
@@ -7,7 +7,7 @@ import gradio as gr
 import sys
 import os
 
-__file__ = "/home/xlab-app-center/"
+
 # 加载基础的语言模型 Horowag_7b
 download(model_repo='SaaRaaS/Horowag_7b',
          output='Horowag_7b')
@@ -37,7 +37,7 @@ qwen_translation_chain = qwen_translation_chain(Qwen_model)
 # 定义音频构建函数
 def voice_builder(context: str):
     # 定义 API 参数
-    program = "SaaRaaS-1300/InternLM_openNotebook/Chatty_Horo_Voich/VITS-kit/cmd_inference.py"
+    program = "Chatty_Horo_Voich/VITS-kit/cmd_inference.py"
     api_param_args_1 = "-m" 
     api_param_conf_1 = "/home/xlab-app-center/Speaker/VITS_Horo_G_10000R.pth"
     api_param_args_2 = "-c" 
