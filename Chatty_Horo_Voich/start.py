39366ef839b9c68888a609a33756daa755223659
SaaRaaS-1300 <89929854+SaaRaaS-1300@users.noreply.github.com>
2024-02-19 23:16:04 +0800

    🍔版本例行维护🍔

--- Chatty_Horo_Voich/start.py
+++ Chatty_Horo_Voich/start.py
@@ -7,7 +7,7 @@ import gradio as gr
 import sys
 import os
 
-__file__ = "Horowag_7b/"
+__file__ = "Chatty_Horo_Voich"
 # 加载基础的语言模型 Horowag_7b
 download(model_repo='SaaRaaS/Horowag_7b',
          output='Horowag_7b')
@@ -43,7 +43,7 @@ def voice_builder(context: str):
     api_param_args_2 = "-c" 
     api_param_conf_2 = "Speaker/VITS_Horo_Config.json"
     api_param_args_3 = "-o" 
-    api_param_conf_3 = "Horowag_7b"
+    api_param_conf_3 = "Chatty_Horo_Voich/"
     api_param_args_4 = "-l" 
     api_param_conf_4 = "日本語"
     api_param_args_5 = "-t" 
@@ -136,7 +136,7 @@ class Chatty_Horo_Chain:
             用于音频转化
         """
         # 路径
-        return os.path.join(os.path.dirname(__file__), "output.wav")
+        return os.path.join(os.path.dirname(__file__), "/output.wav")
 
 
 # 构建对话模式
