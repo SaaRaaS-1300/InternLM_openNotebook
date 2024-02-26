c77f668e4bcf05ed5329c021497c13c949eaaa97
R. YPeng <89929854+SaaRaaS-1300@users.noreply.github.com>
2024-02-26 23:01:51 +0800

    🌠

--- Chatty_Horo_Voich/start.py
+++ Chatty_Horo_Voich/start.py
@@ -17,6 +17,11 @@ os.system("pip install imageio==2.4.1")
 os.system("pip install moviepy==1.0.1")
 os.system("pip install pyopenjtalk==0.3.3")
 os.system("cp open_jtalk_dic_utf_8-1.11.tar.gz /usr/local/share/python/.pyenv/versions/3.9.16/lib/python3.9/site-packages/pyopenjtalk/")
+os.chdir('/usr/local/share/python/.pyenv/versions/3.9.16/lib/python3.9/site-packages/pyopenjtalk/')
+os.system("tar -zxvf open_jtalk_dic_utf_8-1.11.tar.gz")
+print("声音编译环境构建完成...")
+
+# 回归任务环境
 os.chdir('Chatty_Horo_Voich/VITS-kit/monotonic_align')
 os.system("mkdir monotonic_align")
 os.system("python setup.py build_ext --inplace")
